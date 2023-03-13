522e32497cc49947386165db72f209fc9302a04a
Vivian Xu <hi@vivianxu.com>
2023-03-13 22:10:47 +1100

    🐛 Load external stylesheets in Document

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,14 +4,6 @@ import Footer from "@/components/Footer";
 
 const Layout = ({ children }) => (
   <>
-    <Head>
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
-        crossorigin="anonymous"
-      />
-    </Head>
     <Header />
     {children}
     <Footer />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,24 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
+          crossOrigin="anonymous"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Poppins:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <Script
